972a89b3135ab72da99a546eb40a8c50454f043d
Loooai10 <honey_moonn@yahoo.com>
2022-10-17 16:58:59 +0300

    updated

--- controllers/dinosaurs.js
+++ controllers/dinosaurs.js
@@ -4,10 +4,21 @@ const fs = require('fs')
 
 
 router.get('/', (req,res)=>{
+    // pull in JSON data
     let dinosaurs = fs.readFileSync('./dinosaurs.json');
+    // turn the json data into a JS object
     let dinoData = JSON.parse(dinosaurs);
-    console.log(dinoData)
-    res.render('dinosaurs/index', {myDinos: dinoData});
+    
+    let nameFilter = req.query.nameFilter
+    // if the user searched for something
+    if(nameFilter){
+    dinoData = dinoData.filter(dino=>{
+        return dino.name.tolowerCase()===nameFilter.tolowerCase()
+    // console.log(dinoData)
+    // }
+    })
+}
+    res.render('dinosaurs/index.ejs', {myDinos: dinoData});
 });
 
 router.get('/new', (req,res)=>{
@@ -23,6 +34,48 @@ router.get('/:idx', (req,res)=>{
 });
 router.post('/', (req, res)=> {
     console.log('This is the Request Bodu:', req.body)
-     res.redirect("/dinosaurs")
+    let dinosaurs = fs.readFileSync('./dinosaurs.json');
+    let dinoData = JSON.parse(dinosaurs);
+
+    dinoData.push(req.body);
+    fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinoData));
+
+
+
+    res.redirect("/dinosaurs")
 })
+// /:idex related to the number of the dinosaur that gonna be deleted
+//
+router.delete('/:idx', (req,res) => {
+    console.log('This is my Req params object', req.params)
+    let dinosaurs = fs.readFileSync('./dinosaurs.json');
+    let dinoData = JSON.parse(dinosaurs);
+    dinoData.splice(req.params.idx, 1)
+    fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinoData));
+    res.redirect('/dinosaurs')
+})
+router.get('/edit/:idx', (req,res)=>{
+    // grab dino data
+    let dinosaurs = fs.readFileSync('./dinosaurs.json');
+    let dinoData = JSON.parse(dinosaurs);
+
+    res.render('dinosaurs/edit', {dino: dinoData[req.params.idx], dinoId: req.params.idx});
+    //Display edit page
+    res.render('dinosaurs/edit', {dino: dinoData[req.params.idx], dinoId: req.params.idx})
+})
+router.put('/:dinoId', (req,res)=>{
+    //grab all dino data VVV
+    let dinosaurs = fs.readFileSync('./dinosaurs.json');
+    // Parse Json data into JS Object VV
+    let dinoData = JSON.parse(dinosaurs);
+    // update our dinosaurs with form data
+    dinoData[req.params.dinoId].name = req.body.name;
+    dinoData[req.params.dinoId].type = req.body.type;
+    //update our Json file with new data
+    fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinoData));
+   // redirect to home page
+    res.redirect('/dinosaurs');
+
+})
+
 module.exports = router;
--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 //npm i express express-ejs-layouts
 const express = require('express')
+port = 3500
+
 const ejsLayouts = require('express-ejs-layouts')
 const app = express()
 const fs = require('fs');
 
 app.set('view engine', 'ejs')
 app.use(ejsLayouts)
-app.use(express.urlencoded({extended: false}));
 
+app.use((req,res, next) => {
+    console.log('our own middleware')
+    console.log(`Request for ${req.method} at ${req.path}`)
+    next()
+})
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
+
+app.use(express.urlencoded({extended: false}));
 app.use('/dinosaurs', require('./controllers/dinosaurs'))
 
 app.listen(3500, ()=> {
